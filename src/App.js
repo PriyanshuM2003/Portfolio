c004864d28b65bfa2e99c0ea4b0ac55aaff44631
Priyanshu-Butterpaper1 <priyanshu.mishra@butterpaper.tech>
2023-10-04 15:19:55 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import "aos/dist/aos.css";
 import Navbar from "./Components/Navbar";
 import Home from "./Components/Home";
 import About from "./Components/About";
